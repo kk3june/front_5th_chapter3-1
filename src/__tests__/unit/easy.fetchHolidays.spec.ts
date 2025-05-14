e5be134ce147c0d1bc67929001324d3127b9674d
3jun <kk3june@gmail.com>
2025-05-15 07:05:19 +0900

    feat. fetchHolidays 테스트 코드 작성

--- src/__tests__/unit/easy.fetchHolidays.spec.ts
+++ src/__tests__/unit/easy.fetchHolidays.spec.ts
@@ -1,9 +1,28 @@
 import { fetchHolidays } from '../../apis/fetchHolidays';
 
 describe('fetchHolidays', () => {
-  it('주어진 월의 공휴일만 반환한다', () => {});
+  it('주어진 월의 공휴일만 반환한다', () => {
+    const date = new Date('2025-03-01');
+    const result = fetchHolidays(date);
+    expect(result).toEqual({
+      '2025-03-01': '삼일절',
+    });
+  });
 
-  it('공휴일이 없는 월에 대해 빈 객체를 반환한다', () => {});
+  it('공휴일이 없는 월에 대해 빈 객체를 반환한다', () => {
+    const date = new Date('2025-02-01');
+    const result = fetchHolidays(date);
+    expect(result).toEqual({});
+  });
 
-  it('여러 공휴일이 있는 월에 대해 모든 공휴일을 반환한다', () => {});
+  it('여러 공휴일이 있는 월에 대해 모든 공휴일을 반환한다', () => {
+    const date = new Date('2025-01-01');
+    const result = fetchHolidays(date);
+    expect(result).toEqual({
+      '2025-01-01': '신정',
+      '2025-01-29': '설날',
+      '2025-01-30': '설날',
+      '2025-01-31': '설날',
+    });
+  });
 });
