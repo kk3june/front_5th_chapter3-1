eff6adc461825235670513d3406a7fd5948e824a
sj.kim <sj.kim@greendote.co.kr>
2025-05-15 13:49:05 +0900

    feat. notificationUtils 테스트 코드 구현

--- src/__tests__/unit/easy.notificationUtils.spec.ts
+++ src/__tests__/unit/easy.notificationUtils.spec.ts
@@ -2,15 +2,158 @@ import { Event } from '../../types';
 import { createNotificationMessage, getUpcomingEvents } from '../../utils/notificationUtils';
 
 describe('getUpcomingEvents', () => {
-  it('알림 시간이 정확히 도래한 이벤트를 반환한다', () => {});
+  it('알림 시간이 정확히 도래한 이벤트를 반환한다', () => {
+    const events: Event[] = [
+      {
+        id: '1',
+        title: '이벤트 1',
+        date: '2025-07-01',
+        startTime: '10:00',
+        endTime: '11:00',
+        description: '이벤트 1 설명',
+        location: '이벤트 1 장소',
+        category: '이벤트 1 카테고리',
+        repeat: { type: 'none', interval: 0 },
+        notificationTime: 30,
+      },
+      {
+        id: '2',
+        title: '이벤트 2',
+        date: '2025-07-01',
+        startTime: '12:00',
+        endTime: '13:00',
+        description: '이벤트 2 설명',
+        location: '이벤트 2 장소',
+        category: '이벤트 2 카테고리',
+        repeat: { type: 'none', interval: 0 },
+        notificationTime: 1,
+      },
+    ];
+    const now = new Date('2025-07-01T09:30:00');
 
-  it('이미 알림이 간 이벤트는 제외한다', () => {});
+    const result = getUpcomingEvents(events, now, []);
+    expect(result).toEqual([events[0]]);
+  });
 
-  it('알림 시간이 아직 도래하지 않은 이벤트는 반환하지 않는다', () => {});
+  it('이미 알림이 간 이벤트는 제외한다', () => {
+    const now = new Date('2025-05-10T13:30:00');
+    const notifiedEvents: string[] = ['1'];
 
-  it('알림 시간이 지난 이벤트는 반환하지 않는다', () => {});
+    const events: Event[] = [
+      {
+        id: '1',
+        title: '이벤트 1',
+        date: '2025-07-01',
+        startTime: '10:00',
+        endTime: '11:00',
+        description: '이벤트 1 설명',
+        location: '이벤트 1 장소',
+        category: '이벤트 1 카테고리',
+        repeat: { type: 'none', interval: 0 },
+        notificationTime: 30,
+      },
+      {
+        id: '2',
+        title: '이벤트 2',
+        date: '2025-07-01',
+        startTime: '12:00',
+        endTime: '13:00',
+        description: '이벤트 2 설명',
+        location: '이벤트 2 장소',
+        category: '이벤트 2 카테고리',
+        repeat: { type: 'none', interval: 0 },
+        notificationTime: 1,
+      },
+    ];
+
+    const result = getUpcomingEvents(events, now, notifiedEvents);
+
+    expect(result).toHaveLength(0);
+  });
+
+  it('알림 시간이 아직 도래하지 않은 이벤트는 반환하지 않는다', () => {
+    const events: Event[] = [
+      {
+        id: '1',
+        title: '이벤트 1',
+        date: '2025-07-01',
+        startTime: '10:00',
+        endTime: '11:00',
+        description: '이벤트 1 설명',
+        location: '이벤트 1 장소',
+        category: '이벤트 1 카테고리',
+        repeat: { type: 'none', interval: 0 },
+        notificationTime: 30,
+      },
+      {
+        id: '2',
+        title: '이벤트 2',
+        date: '2025-07-01',
+        startTime: '12:00',
+        endTime: '13:00',
+        description: '이벤트 2 설명',
+        location: '이벤트 2 장소',
+        category: '이벤트 2 카테고리',
+        repeat: { type: 'none', interval: 0 },
+        notificationTime: 1,
+      },
+    ];
+
+    const now = new Date('2025-07-01T09:30:00');
+    const result = getUpcomingEvents(events, now, []);
+    expect(result).toEqual([events[0]]);
+  });
+
+  it('알림 시간이 지난 이벤트는 반환하지 않는다', () => {
+    const events: Event[] = [
+      {
+        id: '1',
+        title: '이벤트 1',
+        date: '2025-07-01',
+        startTime: '10:00',
+        endTime: '11:00',
+        description: '이벤트 1 설명',
+        location: '이벤트 1 장소',
+        category: '이벤트 1 카테고리',
+        repeat: { type: 'none', interval: 0 },
+        notificationTime: 30,
+      },
+      {
+        id: '2',
+        title: '이벤트 2',
+        date: '2025-07-01',
+        startTime: '12:00',
+        endTime: '13:00',
+        description: '이벤트 2 설명',
+        location: '이벤트 2 장소',
+        category: '이벤트 2 카테고리',
+        repeat: { type: 'none', interval: 0 },
+        notificationTime: 1,
+      },
+    ];
+
+    const now = new Date('2025-07-01T10:30:00');
+    const result = getUpcomingEvents(events, now, []);
+    expect(result).toEqual([]);
+  });
 });
 
 describe('createNotificationMessage', () => {
-  it('올바른 알림 메시지를 생성해야 한다', () => {});
+  it('올바른 알림 메시지를 생성해야 한다', () => {
+    const event: Event = {
+      id: '1',
+      title: '이벤트 1',
+      date: '2025-07-01',
+      startTime: '10:00',
+      endTime: '11:00',
+      description: '이벤트 1 설명',
+      location: '이벤트 1 장소',
+      category: '이벤트 1 카테고리',
+      repeat: { type: 'none', interval: 0 },
+      notificationTime: 30,
+    };
+
+    const result = createNotificationMessage(event);
+    expect(result).toBe('30분 후 이벤트 1 일정이 시작됩니다.');
+  });
 });
